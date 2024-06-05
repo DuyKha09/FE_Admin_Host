fa15760a0674e21e60387e46bdcc9b44bf8e1a58
Nguyen Anh <ntac1k25@gmail.com>
2024-06-05 15:25:38 +0700

    update hostpage

--- src/pages/HostPage/HostPage.jsx
+++ src/pages/HostPage/HostPage.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import Header from '../../components/Host/Header'
 import Home from '../../components/Host/Home'
 import Sidebar from '../../components/Host/Sidebar'
+import { useNavigate } from 'react-router-dom'
+import { LocalStorage } from '../../utils/LocalStorage'
 
 function App() {
+    const navigate = useNavigate;
+    const [collapsed, setCollapsed] = useState(false);
+    useEffect(() => {
+      const role = LocalStorage.getRole();
+      if (role !== "host") {
+        navigate("/forbidden");
+        LocalStorage.clearToken();
+      }
+    }, [navigate]);
+
     const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
   
     const OpenSidebar = () => {
